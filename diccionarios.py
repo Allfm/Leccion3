e61284d8076c29bcc42831868469f6dc5f678889
Allfm <124815621+Allfm@users.noreply.github.com>
2023-02-09 03:02:20 +0000

    Leccion.3

---
+++ diccionarios.py
@@ -0,0 +1,25 @@
+#lista_de_hobbies = "Leo, motos", "Nacho, videojuegos", "Allan, gym", "Cristian, deporte de contacto", "Carlos, bici", "Jose, futbol", "Ithamar, comer"
+diccionario = {"Leo" : "motos",
+               "Nacho" : "videojuegos",
+               "Allan" : "gym",
+               "Cristian" : "deporte de contacto",
+               "Carlos" : "bici", 
+               "Jose" : "futbol",
+               "Ithamar" : "comer"}
+#print(diccionario["Allan"])
+
+diccionario["Kevin"]="leer"
+#print(diccionario)
+
+
+menu = {"Hamburguesa" :
+       {"Ingredientes" : ["pan", "tomate", "carne", "lechuga"],
+        "Precio" : 1500}
+        
+        
+        
+        
+        
+        }
+ }
+print(menu["Hamburguesa"]["Ingredientes"][2])
---
+++ listas.py
@@ -0,0 +1,15 @@
+#lista_del_mago = "patas de araña, lengua de rana, ojo de buey, ala de muercielago"
+#print(lista_del_mago)
+
+
+lista = [0, 1, 2, 3, 4]
+
+lista_del_mago = ["patas de araña", "lengua de rana", "ojo de buey", "ala de muercielago"]
+#print(lista_del_mago)
+#print(lista_del_mago[0])
+#print(lista_del_mago[1:4])
+
+lista_del_mago.append("piel de serpiente")
+
+del lista_del_mago[2]
+print(lista_del_mago)
